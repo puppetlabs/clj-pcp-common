fdc929169c9b7d4d3d82caaa4ee802a2ddb9ef9b
Richard Clamp <richardc@unixbeard.net>
2015-03-27 16:58:08 +0000

    lein-release plugin: preparing 0.0.1 release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/cthun-message "0.0.1-SNAPSHOT"
+(defproject puppetlabs/cthun-message "0.0.1"
   :description "Message serialisation codec for cthun"
   :url "https://github.com/puppetlabs/clj-cthun-message"
   :license {:name ""
