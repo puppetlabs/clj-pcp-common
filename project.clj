54004f762f47824e8ec1cb4b6a51b16073a70f0a
Jenkins CI <ci@puppetlabs.com>
2015-10-21 08:23:54 -0700

    Version 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/pcp-common "0.5.0-SNAPSHOT"
+(defproject puppetlabs/pcp-common "0.5.0"
   :description "Common protocol components for PCP"
   :url "https://github.com/puppetlabs/clj-pcp-common"
   :license {:name "Apache License, Version 2.0"
